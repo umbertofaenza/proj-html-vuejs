29e8759b3cf18567d446e8c052ec85d40451cd76
umbertofaenza <umberto.faenza@gmail.com>
2023-09-20 11:28:28 +0200

    feat: populate categoryCards dinamically (no imgs)

--- src/components/other_components/SpecialCard.vue
+++ src/components/other_components/SpecialCard.vue
@@ -1,5 +1,56 @@
-<script></script>
+<script>
+export default {
+  props: {
+    cardData: Array,
+  },
+};
+</script>
 
-<template></template>
+<template>
+  <div class="col">
+    <!-- card -->
+    <div class="special-card d-flex align-items-end">
+      <!-- card text -->
+      <div class="card-text">
+        <!-- title -->
+        <div class="card-title">
+          {{ cardData.title }}
+        </div>
+        <!-- button -->
+        <div class="card-btn">
+          <a :href="cardData.href">Shop Now</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "../../style/_partials/variables.scss";
+
+a {
+  text-decoration: none;
+  color: currentColor;
+}
+
+.special-card {
+  height: 550px;
+  background-color: black;
+}
+
+.card-text {
+  width: 100%;
+  background-color: rgba($color: $primary-color, $alpha: 0.5);
+  padding: 1.5rem;
+
+  .card-title {
+    font-size: 1.2rem;
+    font-weight: 400;
+  }
+
+  .card-btn {
+    color: $secondary-color-light;
+    text-decoration: underline;
+  }
+}
+</style>
--- src/components/section_components/SpecialCategory.vue
+++ src/components/section_components/SpecialCategory.vue
@@ -1,8 +1,32 @@
-<script></script>
+<script>
+// import components
+import SpecialCard from "../other_components/SpecialCard.vue";
+
+// import data
+import { store } from "../../data/store";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+  components: { SpecialCard },
+};
+</script>
 
 <template>
   <section id="special-category">
+    <!-- title -->
     <div class="section-title">Special Category</div>
+    <!-- card -->
+    <div class="row row-cols-3">
+      <SpecialCard
+        v-for="card in this.store.specialCategoryData"
+        :cardData="card"
+      />
+    </div>
   </section>
 </template>
 
@@ -24,6 +48,7 @@
     border-bottom: 2px solid $secondary-color;
     width: 3rem;
     margin: 0.5rem auto;
+    margin-bottom: 1.5rem;
   }
 }
 </style>
