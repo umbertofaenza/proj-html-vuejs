028cb47845864d2534b2c36f63f69b05318432ff
umbertofaenza <umberto.faenza@gmail.com>
2023-09-21 12:49:21 +0200

    feat: create component for quote jumbo/slider

--- src/components/main_components/Main.vue
+++ src/components/main_components/Main.vue
@@ -5,9 +5,17 @@ import SpecialCategory from "../section_components/SpecialCategory.vue";
 import OurProducts from "../section_components/OurProducts.vue";
 import DOTD from "../section_components/DOTD.vue";
 import Blogs from "../section_components/Blogs.vue";
+import QuoteSlider from "../other_components/QuoteSlider.vue";
 
 export default {
-  components: { ShowcaseCard, SpecialCategory, OurProducts, DOTD, Blogs },
+  components: {
+    ShowcaseCard,
+    SpecialCategory,
+    OurProducts,
+    DOTD,
+    Blogs,
+    QuoteSlider,
+  },
 
   props: {
     topShowcaseCardsData: Array,
@@ -114,32 +122,8 @@ export default {
       <DOTD />
     </div>
 
-    <!--# quote jumbo  -->
-    <div id="quote-jumbo">
-      <div class="container d-flex align-items-center">
-        <!-- quote wrapper -->
-        <div
-          class="quote-wrapper d-flex flex-column justify-content-center align-items-center"
-        >
-          <!-- pic -->
-          <div class="profile-pic">
-            <img src="/img/t5-1-100x100_left.jpg" />
-          </div>
-          <!-- name -->
-          <div class="profile-name">
-            <span>Luies Charls</span>
-            <span class="role">(Web Designer)</span>
-          </div>
-          <!-- quote -->
-          <div class="quote">
-            "Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Blanditiis, voluptas. Magnam quod ad maiores maxime. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Ea sapiente, ipsa neque
-            voluptatum at omnis. Lorem ipsum dolor sit amet."
-          </div>
-        </div>
-      </div>
-    </div>
+    <!--# quote jumbo/slider -->
+    <QuoteSlider />
 
     <div class="container">
       <!--# blogs section  -->
@@ -247,41 +231,6 @@ main {
 
 #showcase-bottom {
   padding: 4rem 0;
-}
-
-#quote-jumbo {
-  height: 450px;
-  background-image: url("/img/parallax.jpg");
-  background-size: cover;
-  background-position: center;
-  margin: 3.5rem 0;
-  font-size: 1.2rem;
-
-  .profile-pic {
-    border-radius: 50%;
-    overflow: hidden;
-    border: 2px solid $secondary-color-light;
-  }
-
-  .profile-name {
-    font-weight: 400;
-    margin: 1rem 0;
-  }
-
-  .profile-name .role {
-    color: $secondary-color-light;
-    margin-left: 5px;
-  }
-
-  .quote {
-    max-width: 70%;
-    color: #aaaaaa;
-    text-align: center;
-  }
-
-  .container {
-    height: 100%;
-  }
 }
 
 .sponsors-divider {
---
+++ src/components/other_components/QuoteSlider.vue
@@ -0,0 +1,68 @@
+<script></script>
+
+<template>
+  <div id="quote-jumbo">
+    <div class="container d-flex align-items-center">
+      <!-- quote wrapper -->
+      <div
+        class="quote-wrapper d-flex flex-column justify-content-center align-items-center"
+      >
+        <!-- pic -->
+        <div class="profile-pic">
+          <img src="/img/t5-1-100x100_left.jpg" />
+        </div>
+        <!-- name -->
+        <div class="profile-name">
+          <span>Luies Charls</span>
+          <span class="role">(Web Designer)</span>
+        </div>
+        <!-- quote -->
+        <div class="quote">
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis,
+          voluptas. Magnam quod ad maiores maxime. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Ea sapiente, ipsa neque voluptatum at
+          omnis. Lorem ipsum dolor sit amet."
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../../style/_partials/variables.scss";
+
+#quote-jumbo {
+  height: 450px;
+  background-image: url("/img/parallax.jpg");
+  background-size: cover;
+  background-position: center;
+  margin: 3.5rem 0;
+  font-size: 1.2rem;
+
+  .profile-pic {
+    border-radius: 50%;
+    overflow: hidden;
+    border: 2px solid $secondary-color-light;
+  }
+
+  .profile-name {
+    font-weight: 400;
+    margin: 1rem 0;
+  }
+
+  .profile-name .role {
+    color: $secondary-color-light;
+    margin-left: 5px;
+  }
+
+  .quote {
+    max-width: 70%;
+    color: #aaaaaa;
+    text-align: center;
+  }
+
+  .container {
+    height: 100%;
+  }
+}
+</style>
